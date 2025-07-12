0d556ed27973cc29be5794f6ee9778139bd2978a
MinJae-King <minjaekim7111@naver.com>
2025-07-13 00:41:32 +0900

    ??!

---
+++ src/Num10926.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Num10926 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        String str =  br.readLine();
+
+        bw.write(str + "??!" + "\n");
+        bw.flush();
+
+    }
+}
