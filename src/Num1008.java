85c26b67ed20298dc7323a6054160983962459ed
MinJae-King <minjaekim7111@naver.com>
2025-07-12 17:58:53 +0900

    A / B

---
+++ src/Num1008.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Num1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        double result = a / b;
+        System.out.println(result);
+
+    }
+}
