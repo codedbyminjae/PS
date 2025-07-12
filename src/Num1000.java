eb60830b25da65ce57ae4849e18c41b34f33696d
MinJae-King <minjaekim7111@naver.com>
2025-07-12 17:51:39 +0900

    A + B

---
+++ src/Num1000.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Num1000 {
+    public static void main(String[] args) {
+        Scanner a =  new Scanner(System.in);
+        int n = a.nextInt();
+        int m = a.nextInt();
+
+        System.out.println(n + m);
+    }
+}
