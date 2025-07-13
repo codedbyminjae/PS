fbb5299d701d538eac8952c4fb0f7f485d0deb3c
MinJae-King <minjaekim7111@naver.com>
2025-07-14 00:54:44 +0900

    별 찍기 - 4

---
+++ src/Num2441.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Num2441 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0 ; i < N ; i++) {
+            for (int j = 0 ; j < i; j++) {
+                bw.write(" ");
+            }
+            for (int j = 0 ; j < N-i ; j++) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+        bw.flush();
+
+    }
+}
