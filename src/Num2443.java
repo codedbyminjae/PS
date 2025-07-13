bb7ae4c15313733c168bdf9cae2c5441222d3338
MinJae-King <minjaekim7111@naver.com>
2025-07-14 01:14:05 +0900

    별 찍기 - 6

---
+++ src/Num2443.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Num2443 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = N ; i > 0; i--) {
+            for (int j = 0; j < N-i; j++) {
+                bw.write(" ");
+            }
+            for (int j = 0; j < 2*i-1; j++) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+        bw.flush();
+    }
+}
