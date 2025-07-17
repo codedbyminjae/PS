b6909d86a2f6145796a21f3abcf53523adc603a8
MinJae-King <minjaekim7111@naver.com>
2025-07-17 21:47:10 +0900

    1998년생인 내가 태국에서는 2541년생?!

---
+++ src/Num18108.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Num18108 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+
+        int N = Integer.parseInt(br.readLine());
+
+        bw.write(N-543 + "\n");
+        bw.flush();
+    }
+}
