baa910d235d10d443cc531846e4e13094471bf7b
MinJae-King <minjaekim7111@naver.com>
2025-07-28 16:09:29 +0900

    바구니 뒤집기

---
+++ src/Num10811.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+public class Num10811 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        String[] ball = br.readLine().split(" ");
+
+        int N =  Integer.parseInt(ball[0]);
+        int M =  Integer.parseInt(ball[1]);
+
+        int[] basket = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            basket[i] = i;
+        }
+
+        for (int i = 0; i < M; i++) {
+            String[] change =  br.readLine().split(" ");
+            int a = Integer.parseInt(change[0]);
+            int b = Integer.parseInt(change[1]);
+
+            for(int j = 0; j < (b - a + 1) / 2; j++){
+                int temp = basket[a + j];
+                basket[a + j] = basket[b - j];
+                basket[b - j] = temp;
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            bw.write(basket[i] + " ");
+        }
+        bw.flush();
+
+    }
+}
