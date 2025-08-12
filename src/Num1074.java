41ad6864daa1dd23efe76ae057bd8b594c14d42c
MinJae-King <minjaekim7111@naver.com>
2025-08-12 12:57:10 +0900

    Z - Final Try

--- src/Num1074.java
+++ src/Num1074.java
@@ -20,17 +20,22 @@ public class Num1074 {
             // 정사각형 -> 한 사분면의 크기
             int size = quarter * quarter;
 
+
             if (r < quarter && c >= quarter) { // 1 사분면
                 // 2사분면 값을 모두 지나야 하기에 1 * size
                 result = result + 1 * size;
+                c = c - quarter;
             } else if (r < quarter && c < quarter) { // 2 사분면
 
             } else if (r >= quarter && c < quarter) { // 3 사분면
                 // 1, 2 사분면을 모두 지나간 값 8부터 시작
                 result = result + 2 * size;
+                r = r - quarter;
             } else if (r >= quarter && c >= quarter) { // 4 사분면
                 // 1, 2, 3 사분면을 모두 지난 12부터 값 시작
                 result = result + 3 * size;
+                r = r - quarter;
+                c = c - quarter;
             }
             N--;
         }
