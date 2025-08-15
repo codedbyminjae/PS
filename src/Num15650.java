f196713357ca0512c4ccbcb4a138479d87d74b46
MinJae-King <minjaekim7111@naver.com>
2025-08-15 14:54:30 +0900

    N과 M (2)

---
+++ src/Num15650.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class Num15650 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static int N, M;
+    static int[] pick;
+
+    public static void main(String[] args) throws IOException {
+        String[] first = br.readLine().split(" ");
+        N = Integer.parseInt(first[0]);
+        M = Integer.parseInt(first[1]);
+
+        pick = new int[M];
+        dfs(1, 0);
+        bw.flush();
+    }
+
+    static void dfs (int start, int depth) throws IOException {
+        if (depth == M) {
+            for (int k = 0; k < M; k++) {
+                bw.write(pick[k] +(k + 1 == M ? "\n" : " "));
+            }
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+            pick[depth] = i;
+            dfs(i + 1, depth + 1);
+        }
+    }
+}
