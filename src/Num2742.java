6ce3d32969ec0db102be01c55b59149914164178
MinJae-King <minjaekim7111@naver.com>
2025-08-24 20:07:42 +0900

    기찍 N

---
+++ src/Num2742.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Num2742 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = N; i >= 1; i--) {
+            bw.write(i + "\n");
+        }
+        bw.flush();
+    }
+}
