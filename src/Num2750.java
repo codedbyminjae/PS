e1b5efb858e2a1abd53182c28b8ef7479ab383fd
codedbyminjae <minjaekim7111@naver.com>
2025-08-31 22:09:27 +0900

    수 정렬하기

---
+++ src/Num2750.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class Num2750 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int N =  Integer.parseInt(br.readLine());
+
+        int[] arr =  new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            bw.write(arr[i] + "\n");
+        }
+        bw.flush();
+    }
+}
