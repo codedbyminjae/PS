b2e0c6c746535a370736ffc3753fe4f9c095c931
codedbyminjae <minjaekim7111@naver.com>
2025-11-19 00:33:36 +0900

    배수와 약수

---
+++ src/Num5086.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Num5086 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main (String[] args) throws IOException {
+        while (true) {
+            String[] input = br.readLine().split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+
+            if (a == 0 && b == 0) {
+                break;
+            }
+
+            if (b % a == 0) {
+                bw.write("factor\n");
+            } else if (a % b == 0) {
+                bw.write("multiple\n");
+            } else {
+                bw.write("neither\n");
+            }
+        }
+        bw.flush();
+    }
+}
