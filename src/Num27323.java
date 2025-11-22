fc2d23621f6c198481f768546c080935942c846e
codedbyminjae <minjaekim7111@naver.com>
2025-11-23 01:07:04 +0900

    직사각형

---
+++ src/Num27323.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Num27323 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        // A, B 입력 받기
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+        // 직사각형 넓이 계산
+        int area = A * B;
+
+        // 출력
+        bw.write(area + "\n");
+        bw.flush();
+    }
+}
